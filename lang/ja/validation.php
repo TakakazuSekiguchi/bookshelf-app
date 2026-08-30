4969f799182d133dc127709e6615ea52c0c0aca9
takakazu sekiguchi <aokitakakazu@gmail.com>
2026-08-31 02:19:30 +0900

    Initial commit

---
+++ lang/ja/validation.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'required' => ':attributeは必須です。',
+
+    'email' => ':attributeには有効なメールアドレスを入力してください。',
+
+    'min' => [
+        'string' => ':attributeは:min文字以上で入力してください。',
+    ],
+
+    'max' => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+
+    'confirmed' => ':attributeが一致しません。',
+
+    'attributes' => [
+        'name' => '名前',
+        'email' => 'メールアドレス',
+        'password' => 'パスワード',
+    ],
+
+];
