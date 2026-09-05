63eb29d9b0f16d3870a042976e0e4bb59afdedfa
takakazu sekiguchi <aokitakakazu@gmail.com>
2026-09-05 16:30:59 +0900

    マイグレーション作成

---
+++ database/migrations/2026_09_04_131845_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('author', 100);
+            $table->string('isbn_13', 13);
+            $table->date('published_date')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image_url', 2048)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
