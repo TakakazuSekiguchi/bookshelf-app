d2cd062d623f42e0e362e694ab64c238fcebe7e0
takakazu sekiguchi <aokitakakazu@gmail.com>
2026-09-17 13:34:30 +0900

    FormRequestを作成

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 
 //コントローラー
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\FavoriteController;
 use App\Http\Controllers\ReviewLikeController;
